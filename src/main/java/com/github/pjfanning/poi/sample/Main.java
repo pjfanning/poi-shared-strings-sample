111ddec4bb471a1fcdf3b1bb404210fb4544c48b
PJ Fanning <fanningpj@yahoo.com>
2018-09-05 18:07:19 +0100

    stub main class

---
+++ src/main/java/com/github/pjfanning/poi/sample/Main.java
@@ -0,0 +1,11 @@
+package com.github.pjfanning.poi.sample;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
