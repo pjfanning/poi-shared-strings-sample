4fa1e4f09b50103e2c6805b21244c1c5ca1883ae
PJ Fanning <fanningpj@yahoo.com>
2018-09-05 20:28:39 +0100

    add write sample

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # poi-shared-strings-sample
-Sample demonstrating the use of [poi-shared-strings](https://github.com/pjfanning/poi-shared-strings) when reading xlsx files.
+Sample demonstrating the use of [poi-shared-strings](https://github.com/pjfanning/poi-shared-strings) when reading or writing xlsx files.
 
 `./gradlew clean run`
+
+The build.gradle file has a mainClassName has a default of `com.github.pjfanning.poi.sample.XLSX2CSV`.
+This is the read sample.
+
+Change the mainClassName to `com.github.pjfanning.poi.sample.StreamingWrite` to run the write sample. 
---
+++ src/main/java/com/github/pjfanning/poi/sample/StreamingWrite.java
@@ -0,0 +1,41 @@
+package com.github.pjfanning.poi.sample;
+
+import com.github.pjfanning.poi.xssf.streaming.SXSSFFactory;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.util.UUID;
+
+public class StreamingWrite {
+    private static String OUTPUT_FILENAME = "StreamingWrite.xlsx";
+    private static int ROWS = 1000;
+    private static int COLUMNS = 10;
+
+    public static void main(String[] args) {
+        // the SXSSFFactory ensures that TempFileSharedStringsTable is used instead of the in-memory default
+        // the SXSSFFactory `true` parameter means the temp file data is encrypted
+        // the final SXSSFWorkbook `true` parameter means that SXSSFWorkbook will use shared strings
+        // if you set this `useSharedStringsTable` to false then you don't really need poi-shared-strings
+        try (SXSSFWorkbook wb = new SXSSFWorkbook(new XSSFWorkbook(new SXSSFFactory(true)),
+                SXSSFWorkbook.DEFAULT_WINDOW_SIZE, true, true)) {
+            SXSSFSheet sheet = wb.createSheet("SheetA");
+            for (int r = 0; r < ROWS; r++) {
+                SXSSFRow row = sheet.createRow(r);
+                for (int c = 0; c < COLUMNS; c++) {
+                    SXSSFCell cell = row.createCell(c);
+                    cell.setCellValue(UUID.randomUUID().toString());
+                }
+            }
+            try (FileOutputStream fos = new FileOutputStream(OUTPUT_FILENAME)) {
+                System.out.println("Writing xlsx file to " + OUTPUT_FILENAME);
+                wb.write(fos);
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
